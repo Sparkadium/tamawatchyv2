5e67f67541b52e7e619713756cf5c9fe6b9b8725
Sparkadium <thomas.denley@gmail.com>
2026-04-15 19:27:06 -0400

    Delete tamav2 directory

--- tamav2/hw_wrapper.cpp
+++
@@ -1,4 +0,0 @@
-// Wrapper to compile hw.c as a separate translation unit
-extern "C" {
-  #include "lib/tamalib/hw.c"
-}
--- tamav2/tamalib_wrapper.cpp
+++
@@ -1,4 +0,0 @@
-// Wrapper to compile tamalib.c as a separate translation unit
-extern "C" {
-  #include "lib/tamalib/tamalib.c"
-}
